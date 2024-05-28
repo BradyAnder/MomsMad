73d5174d00dc8dfa018daa00af5644ddd679dafd
BradyAnder <bradyander105@gmail.com>
2024-05-28 14:25:09 -0400

    Improve quality of life of timer
    Changed text color in size as the timer goes down
     when timer reaches zero display a times up and switch scene

--- Moms-Mad_Run!/Assets/Scripts/Timer/CountDownTimer.cs
+++ Moms-Mad_Run!/Assets/Scripts/Timer/CountDownTimer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class CountDownTimer : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class CountDownTimer : MonoBehaviour
     [SerializeField] float startingTime;
 
     [SerializeField] TextMeshProUGUI countDownText;
+    [SerializeField] TextMeshProUGUI timesUpText;
     
 
     // Start is called before the first frame update
@@ -18,6 +20,8 @@ public class CountDownTimer : MonoBehaviour
         currentTime = startingTime;
         countDownText.color = Color.white;
         countDownText.fontSize = 50;
+        timesUpText.gameObject.SetActive(false);
+        countDownText.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -38,20 +42,31 @@ public class CountDownTimer : MonoBehaviour
         else
         {
             countDownText.text = "0";
+            timesUpText.gameObject.SetActive(true);
+            countDownText.gameObject.SetActive(false);
+            Time.timeScale = 0;
+            StartCoroutine(ChangeScene());
         }
     }
 
     void ChangeColorText()
     {
-        if(currentTime < startingTime / 2)
+        if(currentTime < startingTime / 2 + 1)
         {
             countDownText.color = Color.yellow;
-            countDownText.fontSize = 60;
+            countDownText.fontSize = 65;
         }
-        if(currentTime < 10)
+        if(currentTime <= 11)
         {
             countDownText.color = Color.red;
-            countDownText.fontSize = 80;
+            countDownText.fontSize = 85;
         }
+    }
+
+    IEnumerator ChangeScene()
+    {
+        yield return new WaitForSecondsRealtime(1f);
+        Debug.Log("bruh");
+        SceneManager.LoadScene("MainMenu");
     }
 }

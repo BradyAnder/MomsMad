888c3b6b0ca953638594c1b95e9754c9cb72dabd
Cartwrit <cartwrit@sheridancollege.ca>
2024-06-13 22:21:42 -0400

    Fixed integration
    - Made lobby and spawner work together
    - Added simple colour coding to the children
    -

--- Moms-Mad_Run!/Assets/Scripts/Character/PlayerManager.cs
+++ Moms-Mad_Run!/Assets/Scripts/Character/PlayerManager.cs
@@ -1,32 +1,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using static LobbyManager;
 
 public class PlayerManager : MonoBehaviour
 {
-    public GameObject childPrefab;
-    public GameObject momPrefab;
+    public GameObject playerPrefab;
+    public List<GameObject> childSpawnPoints = new List<GameObject>();
+    List<GameObject> availableSpawnPoints = new List<GameObject>();
 
-    // List of child spawn points
-    private List<Vector3> ChildSpawnPoints = new List<Vector3> 
-    { 
-        new Vector3(-18.34f, 1.73f, -13.33f), 
-        new Vector3(-24.45f, 1.73f, -14.57f), 
-        new Vector3(-21.81f, 2.09f, -7.43f), 
-        new Vector3(-24.83f, 1.73f, -2.1f), 
-        new Vector3(-24.83f, 1.73f, -5.74f), 
-        new Vector3(-24.83f, 1.73f, -9.58f), 
-        new Vector3(-18.99f, 1.73f, -2.16f), 
-        new Vector3(-14.81f, 1.73f, 2.16f), 
-        new Vector3(-18.69f, 1.73f, -6.79f) 
-    };
+    public Material[] childColourMats;
 
-    // The mom position is set at x = -5.82, y =1.91, z = -12.97 with rotation 0,0,0
-    private Vector3 MomSpawnPoint = new Vector3(-5.82f, 1.91f, -12.97f);
+    // The mom spawnPoint
+    public GameObject MomSpawnPoint;
 
     private List<PlayerInput> playerInputs = new List<PlayerInput>();
     private List<int> momOrder = new List<int>();
     private int currentRound = 0;
+
+    private List<Player> playerInfo = new List<Player>();
 
     void Start()
     {
@@ -40,29 +33,15 @@ public class PlayerManager : MonoBehaviour
 
     private void DetectPlayers()
     {
-        // Ensure there are exactly 5 players
-        for (int i = 0; i < 5; i++)
-        {
-            if (i < Gamepad.all.Count)
-            {
-                var playerInput = PlayerInput.Instantiate(childPrefab, controlScheme: "Gamepad", pairWithDevice: Gamepad.all[i]);
-                playerInputs.Add(playerInput);
-                // we print how many players are connected
-                Debug.Log("Player " + i + " connected");
-            }
-            else
-            {
-                Debug.LogError("Not enough gamepads connected!");
-                return;
-            }
-        }
+        playerInfo = LobbyManager.Instance.GetPlayers();
+
     }
 
     private void DetermineMomOrder()
     {
         // Create a list of player indices and shuffle it to determine the mom order
         List<int> playerIndices = new List<int>();
-        for (int i = 0; i < playerInputs.Count; i++)
+        for (int i = 0; i < playerInfo.Count; i++)
         {
             playerIndices.Add(i);
         }
@@ -78,7 +57,7 @@ public class PlayerManager : MonoBehaviour
     void StartRound()
     {
         // Check for end of game
-        if (currentRound >= playerInputs.Count)
+        if (currentRound >= playerInfo.Count)
         {
             // End of game
             Debug.Log("All rounds completed!");
@@ -88,45 +67,71 @@ public class PlayerManager : MonoBehaviour
         // Get the mom player index for this round
         int momPlayerIndex = momOrder[currentRound];
 
-        // Create a list of available child spawn points
-        List<Vector3> availableChildSpawnPoints = new List<Vector3>(ChildSpawnPoints);
+        //Reset the available spawn points to the entire list
+        availableSpawnPoints =childSpawnPoints;
 
-        for (int i = 0; i < playerInputs.Count; i++)
+        //Crude index to track number of child players spawned for changing colours
+        int colourIndex = 0;
+
+        for (int i = 0; i < playerInfo.Count; i++)
         {
             if (i == momPlayerIndex)
             {
-                SpawnMom(playerInputs[i]);
+
+                Debug.Log("Spawned Mom");
+               SpawnMom(playerInfo[i]);
             }
             else
             {
-                SpawnChild(playerInputs[i], availableChildSpawnPoints);
+
+                Debug.Log("Spawned Child");
+                SpawnChild(playerInfo[i], availableSpawnPoints, colourIndex);
+
+                colourIndex++;
             }
         }
+        colourIndex = 0;
+
         currentRound++;
     }
 
-    void SpawnMom(PlayerInput player)
+    void SpawnMom(Player player)
     {
-        momPrefab.transform.position = MomSpawnPoint;
-        momPrefab.transform.rotation = new Quaternion(0, 0, 0, 0);
-        // player.SwitchCurrentControlScheme("Mom");
-        // player.isMom = true;
-        player.gameObject.name = "Mom";
+        //Instantiate a new player and recognize it's Mom and Child objects
+        GameObject newObj = Instantiate(playerPrefab, MomSpawnPoint.transform.position, Quaternion.identity);
+        GameObject momObj = newObj.GetComponentInChildren<MoveMom>().gameObject;
+        GameObject childObj = newObj.GetComponentInChildren<MoveSlideChild>().gameObject;
+
+        //Turn off the child object
+        childObj.SetActive(false);
+
+        //Set the correct device to this player
+        PlayerInput currentPlayer = momObj.GetComponent<PlayerInput>();
+        currentPlayer.SwitchCurrentControlScheme("controller", player.device);
+ 
     }
 
-    void SpawnChild(PlayerInput player, List<Vector3> availableSpawnPoints)
+    void SpawnChild(Player player, List<GameObject> spawnPoints, int ColourValue)
     {
         // Choose a random spawn point for the child
-        int randomIndex = Random.Range(0, 9);
-        Vector3 spawnPoint = availableSpawnPoints[randomIndex];
-        
-       
-        player.transform.position = spawnPoint;
-        player.transform.rotation = new Quaternion(0, 0, 0, 0);
-        // player.SwitchCurrentControlScheme("Child");
-        // player.isMom = false;
-        player.gameObject.name = "Child";
+        int randomIndex = Random.Range(0, spawnPoints.Count);
+        GameObject spawnPoint = spawnPoints[randomIndex];
+        availableSpawnPoints.Remove(spawnPoint);
 
+        //Instantiate a new player and recognize it's Mom and Child objects
+        GameObject newObj = Instantiate(playerPrefab, spawnPoint.transform.position, Quaternion.identity);
+        GameObject momObj = newObj.GetComponentInChildren<MoveMom>().gameObject;
+        GameObject childObj = newObj.GetComponentInChildren<MoveSlideChild>().gameObject;
+
+        //Turn the mom object off
+        momObj.SetActive(false);
+
+        //Change the childs colour based on which child it is
+        childObj.GetComponent<MeshRenderer>().material = childColourMats[ColourValue];
+
+        //Set the correct device to this player
+        PlayerInput currentPlayer = childObj.GetComponent<PlayerInput>();
+        currentPlayer.SwitchCurrentControlScheme("controller", player.device);
     }
 
     void Update()
@@ -137,8 +142,10 @@ public class PlayerManager : MonoBehaviour
             // We spawn a new child 
             StartRound();
 
-            
+
 
         }
     }
+
+
 }

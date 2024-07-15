fda5717cf688ac4c4348eca7b61dd39cbb3d3f36
BradyAnder <bradyander105@gmail.com>
2024-07-15 11:08:52 -0400

    Started to add a fade in and out when the player has stun Resis

--- Moms-Mad_Run!/Assets/Scripts/Stun/GetStunned.cs
+++ Moms-Mad_Run!/Assets/Scripts/Stun/GetStunned.cs
@@ -9,6 +9,10 @@ public class GetStunned : MonoBehaviour
     public bool canBeStunned;
     public GameObject stunEffect;
     public float stunResistance;
+    public Material mat;
+    public float originalOpacity;
+    public bool doFade = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +20,22 @@ public class GetStunned : MonoBehaviour
         playerInput = GetComponent<PlayerInput>();
         playerInput.enabled = true;
         canBeStunned = true;
+        mat = GetComponent<Renderer>().material;
+        originalOpacity = mat.color.a;
+
+
+    }
+
+    private void Update()
+    {
+        if(doFade)
+        {
+            FadeNow();
+        }
+        else
+        {
+            ResetFade();
+        }
     }
 
     public void StunTest()
@@ -58,5 +78,19 @@ public class GetStunned : MonoBehaviour
             UnstunPlayer();
             Debug.Log("Unstun");
         
+    }
+
+    void FadeNow()
+    {
+        Color currentColor = mat.color;
+        Color smoothColor = new Color(currentColor.r, currentColor.g, currentColor.b, Mathf.Lerp(currentColor.a, .2f, 2 * Time.deltaTime));
+        mat.color = smoothColor;
+    }
+
+    void ResetFade()
+    {
+        Color currentColor = mat.color;
+        Color smoothColor = new Color(currentColor.r, currentColor.g, currentColor.b, Mathf.Lerp(currentColor.a, originalOpacity, 2 * Time.deltaTime));
+        mat.color = currentColor;
     }
 }

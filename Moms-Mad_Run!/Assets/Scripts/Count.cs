4fe5335589b73fa6aad258e6ecdc496bf5a981d2
BradyAnder <bradyander105@gmail.com>
2024-07-16 12:15:00 -0400

    Added timer countdown

--- Moms-Mad_Run!/Assets/Scripts/Count.cs
+++ Moms-Mad_Run!/Assets/Scripts/Count.cs
@@ -1,25 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Count : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI rsCountDown;
+    [SerializeField] float three;
+    [SerializeField] float two;
+    [SerializeField] float one;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(CountDown());
+        rsCountDown.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public IEnumerator CountDown()
     {
+        rsCountDown.text = three.ToString();
         Time.timeScale = 0;
-        yield return new WaitForSecondsRealtime(3);
+        yield return new WaitForSecondsRealtime(1);
+        rsCountDown.text = two.ToString();
+        yield return new WaitForSecondsRealtime(1); 
+        rsCountDown.text = one.ToString();
+        yield return new WaitForSecondsRealtime(1);
+        rsCountDown.gameObject.SetActive(false);
         Time.timeScale = 1;
     }
 }

55c8936a0e97e73f23ef30c79850ad3cd29cd3d4
BradyAnder <bradyander105@gmail.com>
2024-07-20 16:09:14 -0400

    Rounds Work

---
+++ Moms-Mad_Run!/Assets/Scripts/Breakable/BreakableRounds.cs
@@ -0,0 +1,85 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BreakableRounds : MonoBehaviour
+{
+    public GameObject[] roundOne;
+    public GameObject[] roundTwo;
+    public GameObject[] roundThree;
+    public GameObject[] roundFour;
+    public GameObject[] roundFive;
+
+    private List<GameObject[]> rounds;
+    private GameObject[] currentRound;
+
+    public BreakableObject breakableObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rounds = new List<GameObject[]>
+        {
+            roundOne,
+            roundTwo,
+            roundThree,
+            roundFour,
+            roundFive
+        };
+
+        selectRandomRound();
+    }
+
+    private void Update()
+    {
+        if(currentRound != null && IsRoundEmpty(currentRound))
+        {
+            selectRandomRound();
+        }
+           
+    }
+
+    private void selectRandomRound()
+    {
+        if (rounds.Count == 0)
+        {
+            return;
+        }
+
+        int randomIndex = Random.Range(0, rounds.Count);
+        currentRound = rounds[randomIndex];
+        EnableRound(currentRound);
+        rounds.RemoveAt(randomIndex);
+
+    }
+
+    private bool IsRoundEmpty(GameObject[] round)
+    {
+        foreach (GameObject obj in round)
+        {
+            if (obj != null && obj.activeSelf)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void EnableRound(GameObject[] round)
+    {
+        foreach (GameObject obj in round)
+        {
+            if (obj != null)
+            {
+                obj.SetActive(true);
+                BreakableObject breakable = obj.GetComponent<BreakableObject>();
+                if (breakable != null)
+                {
+                    breakable.isBreakable = true;
+                }
+            }
+        }   
+    }
+}

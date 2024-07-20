4947fcfeb4c85dcc5307453a095f12733d380cae
BradyAnder <bradyander105@gmail.com>
2024-07-20 15:21:06 -0400

    Added outline toggle for breakable objects

--- Moms-Mad_Run!/Assets/Scripts/Breakable/BreakableObject.cs
+++ Moms-Mad_Run!/Assets/Scripts/Breakable/BreakableObject.cs
@@ -11,16 +11,34 @@ public class BreakableObject : MonoBehaviour
     public ScoreManager scoreManager;
     private PlayerManager playerManager;
 
+    public Material outline;
+    public Material[] materials;
+    public bool isBreakable = false;
+
 
     public int scoreValue;
 
     private void Start()
     {
+
+        Renderer renderer = GetComponent<Renderer>();
+        if(renderer != null)
+        {
+            materials = renderer.materials;
+            outline = materials.Length > 1 ? materials[1] : null;
+        }
         GameObject childObject = GameObject.Find("Child");
         GameObject scoreManagerObject = GameObject.Find("ScoreManager");
         scoreManager = scoreManagerObject.GetComponent<ScoreManager>();
         playerManager = FindObjectOfType<PlayerManager>();
         if (playerManager == null) { Debug.Log("BreakableScoring->Start: playerManager not found."); return; }
+
+        ToggleOutline(false);
+    }
+
+    private void Update()
+    {
+        breakable();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -28,7 +46,7 @@ public class BreakableObject : MonoBehaviour
         if (collision != null)
         {
             moveScript = collision.gameObject.GetComponent<MoveSlideChild>();
-            if (collision.gameObject.tag == "Player" && moveScript.isSliding == true)
+            if (collision.gameObject.tag == "Player" && moveScript.isSliding == true && isBreakable == true)
             {
                 Instantiate(brokenObject, transform.position, transform.rotation);
                 playerManager.AddScore(collision.gameObject, scoreValue);
@@ -47,5 +65,34 @@ public class BreakableObject : MonoBehaviour
        GameObject scorePopUP = Instantiate(ScoreTextPopUp, transform.position, Quaternion.identity);
        TMP_Text scoreText = scorePopUP.GetComponentInChildren<TMP_Text>();
        scoreText.text = scoreValue.ToString();
+    }
+
+    void breakable()
+    {
+        if (isBreakable == true)
+        {
+            ToggleOutline(true);
+        }
+        else
+        {
+            ToggleOutline(false);
+        }
+    }
+
+    public void ToggleOutline (bool enable)
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer != null && outline != null)
+        {
+            if (enable)
+            {
+                materials[1] = outline; // Enable outline
+            }
+            else
+            {
+                materials[1] = null; // Disable outline
+            }
+            renderer.materials = materials;
+        }
     }
 }

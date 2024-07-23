18007b509db58563294c03cd95ef251c802c9d46
Lanth139 <pf.zhang@mail.utoronto.ca>
2024-07-22 20:46:21 -0400

    Add simple arrow attack indicator for mom

--- Moms-Mad_Run!/Assets/Scripts/Slipper/SlipperPowerUp.cs
+++ Moms-Mad_Run!/Assets/Scripts/Slipper/SlipperPowerUp.cs
@@ -13,6 +13,17 @@ public class SlipperPowerUp : MonoBehaviour
         if (!other.tag.Equals("Mom")) { return; }
         SlipperThrow slipperThrow = other.GetComponent<SlipperThrow>();
         if (slipperThrow == null) { Debug.Log("SlipperPowerUp->OnTriggerEnter:Mom missing slipper throw component."); return; }
+        switch (powerUpMode) {
+            case (PowerUpMode.SMG):
+                slipperThrow.powerUpTime = 4f;
+                break;
+            case (PowerUpMode.Bouncy):
+                slipperThrow.powerUpTime = 6f;
+                break;
+            case (PowerUpMode.FanShape):
+                slipperThrow.powerUpTime = 6f;
+                break;
+        }
         slipperThrow.GetPowerUp(powerUpMode);
         PowerupSpawner spawner = FindObjectOfType<PowerupSpawner>();
         spawner.isPowerupInField = false;
